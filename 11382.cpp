ba53873d0dab876242cd1595dcd4717598b33abe
SangDo.L <126847724+twoupload@users.noreply.github.com>
2023-05-10 02:33:46 +0900

    Add files via upload

---
+++ 11382.cpp
@@ -0,0 +1,14 @@
+// 백준 11382번
+// 꼬마 정민 - 꼬마 정민이는 이제 A + B 정도는 쉽게 계산할 수 있다. 이제 A + B + C를 계산할 차례이다!
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	long long a, b, c;
+
+	cin >> a >> b >> c;
+	cout << a + b + c;
+
+	return 0;
+}
